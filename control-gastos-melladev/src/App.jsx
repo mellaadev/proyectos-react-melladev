b3f005d94bbaa6b5a49788e194bb2ee1da7ab70b
Fran Mella <92534421+mellaadev@users.noreply.github.com>
2022-01-10 21:49:59 +0100

    Añadiendo Modal, creando componente de gasto, listado de gastos, mensajes, modal, helper de funciones, almacenando gasto

--- control-gastos-melladev/src/App.jsx
+++ control-gastos-melladev/src/App.jsx
@@ -1,43 +1,78 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Header from './components/Header'
+import Modal from './components/Modal'
+import ListadoGastos from './components/ListadoGastos'
+import { generarId } from './helpers'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [ presupuesto, setPresupuesto ] = useState(0)
+
+  const [ isValidPresupuesto, setIsValidPresupuesto ] = useState(false)
+
+  const [ modal, setModal ] = useState(false)
+  const [ animarModal, setAnimarModal ] = useState(false)
+
+  const [ gastos, setGastos ] = useState([])
+  
+  const handleNuevoGasto = () => {
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500);
+  }
+
+  const guardarGasto = gasto => {
+      gasto.id = generarId()
+      gasto.fecha = Date.now()
+      setGastos([...gastos, gasto])
+
+      setAnimarModal(false)
+
+      setTimeout(() => {
+          setModal(false)
+      }, 500);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <ListadoGastos 
+              gastos={gastos}
+            />
+          </main>
+
+          <div className='nuevo-gasto'>
+            <img 
+              src={IconoNuevoGasto}
+              alt='icono nuevo gasto'
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+      )}
+
+      {modal && 
+        <Modal 
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+        />
+      }
+
+      
     </div>
   )
 }
---
+++ control-gastos-melladev/src/components/Gasto.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Gasto = ({ gasto }) => {
+    
+    const { nombre, cantidad, categoria, id} = gasto;
+
+    return (
+        <div className='gasto sombra'>
+            <div className="contenido-gasto">
+
+                <div className="descripcion-gasto">
+                    <p className="categoria">{categoria}</p>
+                    <p className="nombre-gasto">{nombre}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Gasto
