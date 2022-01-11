792f6c97e2cc022ffb82467c82ee4724b9bc0757
Fran Mella <92534421+mellaadev@users.noreply.github.com>
2022-01-11 12:35:48 +0100

    Swipeable Action, y control de presupuesto con totalGastos, totalDisponible con operaciones

--- control-gastos-melladev/src/App.jsx
+++ control-gastos-melladev/src/App.jsx
@@ -37,8 +37,9 @@ function App() {
   }
   
   return (
-    <div>
+    <div className={modal ? 'fijar' : ''}>
       <Header
+        gastos={gastos}
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
